be1c0d4d439741cf1995fd7299c8b752f8b06d66
Andrei Tiurin <tyrandr@list.ru>
2026-02-20 00:55:10 +0300

    tests fixses final

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from src.product import Product
+from src.category import Category
+
+
+@pytest.fixture
+def first_category():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации",
+        products=[
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+        ]
+    )
+
+
+@pytest.fixture
+def second_category():
+    return Category(
+        name="Телевизоры",
+        description="Современный телевизор",
+        products=[Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)])
+
+
+@pytest.fixture
+def product():
+    return Product(name="Iphone 15", description="512GB, Gray space", price=210000.0, quantity=8)
